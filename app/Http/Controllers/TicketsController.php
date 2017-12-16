da9c570c59239ae37f8309bde1560a6231cbfaab
Norwin Roosen <github@nroo.de>
2017-12-16 13:27:46 +0100

    fix image path (maybe?)

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -56,7 +56,7 @@ class TicketsController extends Controller
         }
 
         $file = Input::file('image');
-        $file->move(public_path('img/tickets' . $ticket->image));
+        $file->move(public_path($ticket->image));
 
         $this->update($request, $ticket);
     }
